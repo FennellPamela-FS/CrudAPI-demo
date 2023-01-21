9b25dcc9f122eba13b093a0632a104b95eab7a72
FennellPamela-FS <pyfennell@student.fullsail.edu>
2023-01-21 00:03:47 -0500

    Update server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //   bare bones for our basic server
 const express = require('express');
 require('dotenv').config();
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
 
@@ -12,13 +12,13 @@ const PORT = process.env.PORT || 8000;
 
 const studentRouter = require('./routes/students');
 
-// const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL;
 
-// mongoose.connect(DATABASE_URL, { useNewURLParser: true });
-// mongoose.set('strictQuery', false);
-// const db = mongoose.connection;
-// db.on('error', error => console.error(error))
-// db.once('open', () => console.log('Database connected established'))
+mongoose.connect(DATABASE_URL, { useNewURLParser: true });
+mongoose.set('strictQuery', false);
+const db = mongoose.connection;
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Database connected established'))
 
 
 app.use(express.json()); // expect json on all routes after this
