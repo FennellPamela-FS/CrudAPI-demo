64f2c3f953e8add0bda63807209402acfacff816
FennellPamela-FS <pyfennell@student.fullsail.edu>
2023-01-31 17:32:15 -0500

    Fix error in users

--- api/models/user.js
+++ api/models/user.js
@@ -1,13 +1,13 @@
 // Student Schema
-const mongoose = require("mongoose");
-const bcrypt = require('bcrypt')
+import { Schema, model } from "mongoose";
+import { genSalt, hash as _hash } from 'bcrypt';
 
 const validateEmail = (email) => {
     // check for characters in front and behind the @ symbol 
     return (/^\S+@\S+\.\S+$/).test(email)
 }
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
     email: {
         type: String,
         required: 'Email address is required',
@@ -31,11 +31,11 @@ userSchema.pre('save', (next) {
     // if user is new then run or if we are modifying pass then run 
     if (userSchema.isNew || userSchema.isModified('password')) {
         // run hashing and salting
-        bcrypt.genSalt(10, (error, salt) => {
+        genSalt(10, (error, salt) => {
             // salt each time 
             // if error the return error, if no error then run bcrypt hash on the password for each salt
             if (error) { return next(error) }
-            bcrypt.hash(user.password, salt, (error, hash) => {
+            _hash(user.password, salt, (error, hash) => {
                 user.password = hash;
                 next();
             })
@@ -46,4 +46,4 @@ userSchema.pre('save', (next) {
     }
 })
 
-module.exports = mongoose.model("User", userSchema);
+export default model("User", userSchema);
